0df10780728c663ca490708f1069f8f294d1213f
Ryan Conway <ryan@rjc.cc>
2019-07-17 01:23:16 +0100

    Recursively process all items in the nav structure, to allow us to flatten any folders containing only one file, moving the file up a level where the folder would have been.

--- mkdocs_title_plugin/__init__.py
+++ mkdocs_title_plugin/__init__.py
@@ -7,9 +7,28 @@ class MkDocsTitlePlugin(mkdocs.plugins.BasePlugin):
     config_scheme = (
     )
 
+    def _process_nav_items_recursive(self, nav_items):
+        new_nav_items = []
+
+        for nav_item in nav_items:
+            if nav_item.children:
+                if len(nav_item.children) == 1:
+                    new_nav_items.append( nav_item.children[0] )
+                else:
+                    nav_item.children = ( self._process_nav_items_recursive( nav_item.children ) )
+                    new_nav_items.append(nav_item)
+            else:
+                new_nav_items.append(nav_item)
+
+        return new_nav_items
+
     def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
         print("hi")
-        import pdb
-        pdb.set_trace()
+        # import pdb ; pdb.set_trace()
+        # TODO: Edit page.title based on the Markdown here.
 
         return markdown
+
+    def on_nav(self, nav, config, files, **kwargs):
+        nav.items = self._process_nav_items_recursive(nav)
+        return nav
