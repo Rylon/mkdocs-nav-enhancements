6af351fedc2571bb6d9880c1e4007f07059155af
Ryan Conway <ryan@rjc.cc>
2019-07-22 16:23:53 +0100

    Add missing description. Be consistent with quotation marks.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
@@ -8,14 +8,14 @@ setup(
     version='0.9.0',
     author='Ryan Conway',
     author_email='ryan@rjc.cc',
-    description='Enhancements',
+    description='This is a small plugin for the excellent MkDocs project which makes some enhancements to the navigation.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rylon/mkdocs-nav-enhancements",
+    long_description_content_type='text/markdown',
+    url='https://github.com/rylon/mkdocs-nav-enhancements',
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        "mkdocs>=1"
+        'mkdocs>=1'
     ],
     include_package_data=True,
     zip_safe=False,
@@ -25,11 +25,11 @@ setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Topic :: Documentation"
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Documentation'
     ],
 )
